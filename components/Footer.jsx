3c7f65af7e2bfeaf28ee4192c38471e9bb56f065
David Vekony <dave.vekony@gmail.com>
2022-03-31 21:10:51 +0100

    initial setup

---
+++ components/Footer.jsx
@@ -0,0 +1,36 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Shelterer works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Shelterer Plus</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It&apos;s a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>David</p>
+        <p>Presents</p>
+        <p>Shelterer</p>
+        <p>Built with Next.js</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust and Safety</p>
+        <p>Say Hi GitHub</p>
+        <p>Easter Eggs</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
