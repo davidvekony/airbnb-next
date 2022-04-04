ab8ca6256a2a0ed3e721eef7af388362acad7c66
David Vekony <dave.vekony@gmail.com>
2022-04-04 16:33:37 +0100

    add InfoCard for search results

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,9 @@ import { useRouter } from 'next/router'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import { format } from 'date-fns'
+import InfoCard from '../components/InfoCard'
 
-function Search() {
+function Search({ searchResults }) {
   const router = useRouter()
 
   console.log(router.query)
@@ -33,6 +34,22 @@ function Search() {
             <p className="button">Rooms and Beds</p>
             <p className="button">More filters</p>
           </div>
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
         </section>
       </main>
 
@@ -42,3 +59,15 @@ function Search() {
 }
 
 export default Search
+
+export async function getServerSideProps() {
+  const searchResults = await fetch('https://links.papareact.com/isz').then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      searchResults,
+    },
+  }
+}
