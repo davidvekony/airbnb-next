6a4b623b23a3c38a39936e8a41f1df3e7170b453
David Vekony <dave.vekony@gmail.com>
2022-04-04 10:06:18 +0100

    GitHub access token test

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home({ exploreData, cardsData }) {
           </div>
         </section>
         <section>
-          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <h2 className="text-4xl font-semibold py-8">Live Somewhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {cardsData?.map(({ img, title }) => (
               <MediumCard key={img} img={img} title={title} />
